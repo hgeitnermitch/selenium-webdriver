7a00c03a11763a515c8899438076ceb69bb6ec2b
hgeitnermitch <97267014+hgeitnermitch@users.noreply.github.com>
2023-07-25 17:38:55 -0400

    Update regex.py
    Updated regex written with followings apps:
    
    https://regexr.com/
    https://regex101.com/

--- regex.py
+++ regex.py
@@ -2,52 +2,70 @@
 Regex written to extract fields from text strings.
 
 '''
+re.compile(r'([A-Z]+)USD\n')
+
+re.compile(r'((.+)\%')
+
+re.compile(r'([A-Z]+)USD')
+
+re.compile(r'([A-Z]+)USD(?:.|\n)')
+
+re.compile(r'([A-Z]+)USD\n')
+
+re.compile(r'([A-Z]+)USD')
+
+re.compile(r'([A-Z]+)USD(?:.|\n)')
+
+re.compile(r'(.+\n)(.+)\%')
+
+re.compile(r'([A-Z]+)\n')
+
+re.compile(r'([A-Z]+)(?:(.|\n)+)([0-9]+\.[0-9]+)\n([0-9]+\.[0-9]+)\%')
+
+re.compile(r'(.+\/.+)\n.+\n.+\$(.+)\n.+:.(.+)')
+
+re.compile(r'(.+\/.+)\n.(PULLBACK|.+)\n.+\$(.+)\n.+:.(.+)')
+
+re.compile(r'\$(.+)\n.(PULLBACK|.+)\n.+\$(.+)\n.+:.(.+)')
+
+re.compile(r'\$(.+)\n.(PULLBACK|.+\n).Price.\$(.+)\n.+:.(.+)')
+
+re.compile(r'\$(.+)\n.(PULLBACK|.+\n).Price.\$(.+)\n.(Alert Count :.(.+)|.+\n)')
+
+re.compile(r'\$([A-Z]+)\n.+\n.+\n.+(?:.Alert Count.:.)(.+)')
+
+re.compile(r'\$([A-Z]+)\n.+\n.+\n.+(?: Alert Count : )(.+)')
+
+re.compile(r'\$(?P<coin>[A-Z]+)\n.+\n.+(?:\$)(?P<price>.+)\n.+(?: )(?P<ac>.+)')
+
+re.compile(r'\$(?P<coin>[A-Z]+)\n.+\n.(?: Price : \$)(?P<price>.+)\n.+(?: )(?P<ac>.+)')
+
+re.compile(r'(?P<coin> [A-Z]+\/[A-Z]+).+\n.+\n.+\n.+(?: Alert count : )(?P<ac>.+)')
+
+re.compile(r'(?P<ticker>[A-Z]+\/[A-Z]+)\n(?: Price: .)(?P<price>.+)\n(?: Alert Count: )(?P<ac>.+)')
+
+re.compile(r'?P<ticker>[A-Z]+)\n(?: Price: .)(?P<price>.+)\n(?: Alert Count: )(?P<ac>.+)')
+
+re.compile(r'[A-Z]+)\n(?: Price: .)(.+)\n(?: Alert Count: )(.+)')
+
+re.compile(r'[A-Z]+\/[A-Z]+)\n(?: Price: .)(.+)\n(?: Alert count: )(.+)')
+
 re.compile(r'(?<=\$)([A-Z]+)')
 
-re.compile(r'(PULLBACK)')
+re.compile(r'(11/../2022 .... ..)')
+
+re.compile(r'(?<=Alert Count :)(.+)')
 
 re.compile(r'(?<= Price : .)(.+)')
 
 re.compile(r'(\d+:\d+)(?= (?:AM|PM))')
 
-re.compile(r'([A-Z]+)-USD\n\$(.+)\n.+\n.+\n.+\n\+(.+)\%')
+re.compile(r'(?<=T)(\d+:\d+)')
+
+re.compile(r'(PULLBACK)')
 
 re.compile(r'([A-Z]+/[A-Z]+)')
 
-re.compile(r'([A-Z]+)\/[A-Z]+.+\n.+\n.+Price : \$(.+)\n.+Alert count : (.+)')
+re.compile(r'(\$)(.+)')
 
-re.compile(r'(.+\/.+)\n.+\n.+\$(.+)\n.+:.(.+)')
-
-re.compile(r'(.+)\%')
-
-re.compile(r'([A-Z]+)-USD\n\$(.+)\n.+\n.+\n.+\n\+(.+)\%')
-
-re.compile(r'(?<=Ticker: .)([A-Z]+)')
-
-re.compile(r'(?<= Alert Count :)(..)')
-
-re.compile(r'(?<= Price : .)(.....)')
-
-re.compile(r'(?<=T)(\d+:\d+)')
-
-re.compile(r'1\n([A-Z]+)')
-
-re.compile(r'(.+\n)(.+)\%')
-
-re.compile(r'20\n([A-Z]+)')
-
-re.compile(r'(.+\n)(.+)\%')
-
-re.compile(r'40\n([A-Z]+)')
-
-re.compile(r'(.+)\%')
-
-re.compile(r'([A-Z]+)\/USDT')
-
- re.compile(r'(?: Alert Count : )(.+)')
-
- re.compile(r'([A-Z]+/[A-Z]+)')
-
- re.compile(r'([A-Z]+)\n(.+)\%')
-
- re.compile(r'\$([A-Z]+)\n.+\n.+\n.+(?:Alert Count : )(.+)')
+re.compile(r'(?:\$| Alert Count : )(.+)')
